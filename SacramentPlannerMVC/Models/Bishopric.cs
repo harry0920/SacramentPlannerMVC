cdae9ba7a39a1d95cba48ceaca03e77212b52f12
Zachary Wilson <wilsonzachary634@gmail.com>
2018-04-05 09:02:28 -0600

    Initial Models

---
+++ SacramentPlannerMVC/Models/Bishopric.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SacramentPlannerMVC.Models
+{
+    public class Bishopric
+    {
+        [StringLength(100)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ SacramentPlannerMVC/Models/Meeting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SacramentPlannerMVC.Models
+{
+    public class Meeting
+    {
+        [Required]
+        public DateTime Date { get; set; }
+        
+        [Range(1, 341)]
+        [Display(Name = "Opening Hymn")]
+        [Required]
+        public int OpeningHymn { get; set; }
+
+        [Range(1, 341)]
+        [Display(Name = "Sacrament Hymn")]
+        [Required]
+        public int SacramentHymn { get; set; }
+
+        [Range(1, 341)]
+        [Display(Name = "Intermediate Hymn")]
+        public int? IntermediateHymn { get; set; }
+
+        [Range(1, 341)]
+        [Display(Name = "Closing Hymn")]
+        [Required]
+        public int ClosingHymn { get; set; }
+        
+        [Display(Name = "Opening Prayer")]
+        [StringLength(100)]
+        [Required]
+        public string OpeningPrayer { get; set; }
+
+        [Display(Name = "Closing Prayer")]
+        [StringLength(100)]
+        [Required]
+        public string ClosingPrayer { get; set; }
+
+        [Required]
+        public Bishopric Conductor { get; set; }
+    }
+}
