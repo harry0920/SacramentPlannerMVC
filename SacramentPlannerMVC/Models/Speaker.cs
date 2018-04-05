13db8fc53144d422b18ae5f3213a2924785301ef
Zachary Wilson <wilsonzachary634@gmail.com>
2018-04-05 09:05:26 -0600

    Speaker Commit

---
+++ SacramentPlannerMVC/Models/Speaker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SacramentPlannerMVC.Models
+{
+    public class Speaker
+    {
+        [StringLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public Meeting Meeting { get; set; }
+    }
+}
