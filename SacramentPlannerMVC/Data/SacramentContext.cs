9edc9e559b7e92a65057e83edbb6f2851dc106bb
Zachary Wilson <wilsonzachary634@gmail.com>
2018-04-05 09:25:36 -0600

    Added db initalizer, the context class, and update the connection string.

---
+++ SacramentPlannerMVC/Data/SacramentContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlannerMVC.Data
+{
+    public class SacramentContext
+    {
+    }
+}
