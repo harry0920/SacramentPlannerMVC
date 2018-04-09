eef541ee38117dbdf11c8a1930a929866f8a2913
Zachary Wilson <wilsonzachary634@gmail.com>
2018-04-09 14:32:35 -0600

    Contexts

--- SacramentPlannerMVC/Data/DbInitializer.cs
+++ SacramentPlannerMVC/Data/DbInitializer.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using SacramentPlannerMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SacramentPlannerMVC.Data
 {
-    public class DbInitializer
+    public static class DbInitializer
     {
+        public static void Initialize(SacramentContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Bishoprics.Any())
+            {
+                return;
+            }
+
+            var bishoprics = new Bishopric[]
+            {
+                new Bishopric{Name="Brother Thayne",IsActive=true},
+                new Bishopric{Name="Zach Wilson",IsActive=true},
+                new Bishopric{Name="Tad Cooper",IsActive=true}
+            };
+            foreach (Bishopric b in bishoprics)
+            {
+                context.Bishoprics.Add(b);
+            }
+            context.SaveChanges();
+        }
     }
 }
--- SacramentPlannerMVC/Data/SacramentContext.cs
+++ SacramentPlannerMVC/Data/SacramentContext.cs
@@ -1,11 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using SacramentPlannerMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SacramentPlannerMVC.Data
 {
-    public class SacramentContext
+    public class SacramentContext : DbContext
     {
+        public SacramentContext(DbContextOptions<SacramentContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Bishopric> Bishoprics { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
+        public DbSet<Speaker> Speakers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Bishopric>().ToTable("Bishopric");
+            modelBuilder.Entity<Meeting>().ToTable("Meeting");
+            modelBuilder.Entity<Speaker>().ToTable("Speaker");
+        }
     }
 }
