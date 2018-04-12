cb7c3cfe4f433e74fcd6ec61061454cae0ab8c5a
theswade <samwade1492@gmail.com>
2018-04-11 22:52:53 -0600

    Unlimited boxes with label

--- SacramentPlannerMVC/wwwroot/js/site.js
+++ SacramentPlannerMVC/wwwroot/js/site.js
@@ -1,9 +1,16 @@
 ﻿// Write your JavaScript code.
 
-
+var count = 0;
 function Add_Speaker()
 {
-    var textbox = document.createElement('input');
+    
+    count++;
+    var label, textbox;
+    label = document.createElement('label');
+    label.appendChild(document.createTextNode('Enter Speaker:'));
+    textbox = document.createElement('input');
     textbox.type = 'text';
-    document.getElementById('add_speaker').appendChild(textbox) ;
+    textbox.id = 'speaker' + count;
+    label.appendChild(textbox);
+    document.getElementById('add_speaker').appendChild(label);
 }
