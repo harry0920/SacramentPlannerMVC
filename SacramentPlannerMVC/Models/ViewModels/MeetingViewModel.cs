50d91e3b71d2ed5c9bcc7a336ebb83a02a01e162
Zachary Wilson <wilsonzachary634@gmail.com>
2018-04-13 09:20:14 -0600

    Removed create speaker button in create meeting

---
+++ SacramentPlannerMVC/Models/ViewModels/MeetingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlannerMVC.Models.ViewModels
+{
+    public class MeetingViewModel
+    {
+        public IEnumerable<Meeting> Meetings { get; set; }
+        public IEnumerable<Speaker> Speakers { get; set; }
+        public Speaker Speaker { get; set; }
+    }
+}
---
+++ SacramentPlannerMVC/Models/ViewModels/SpeakerViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlannerMVC.Models.ViewModels
+{
+    public class SpeakerViewModel
+    {
+        public Meeting Meeting { get; set; }
+        public Speaker Speaker { get; set; }
+    }
+}
